8ff7718f72c85b212a32db1db858135d4a449f04
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-12-14 15:57:31 +0500

    Design

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Calculator from './components/Calculator';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>PWA Calculator</h1>
-      </header>
       <main>
         <Calculator />
       </main>
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
-import Button from './Button';
-import Display from './Display';
+import React, { useState } from "react";
+import Button from "./Button";
+import Buttonop from "./Buttonop";
+import Display from "./Display";
 
 const Calculator = () => {
-  const [result, setResult] = useState('0');
-  
+  const [result, setResult] = useState("0");
+
   const handleClick = (value) => {
-    if (value === '=') {
+    if (value === "=") {
       try {
         setResult(eval(result).toString());
       } catch (error) {
-        setResult('Error');
+        setResult("Error");
       }
-    } else if (value === 'C') {
-      setResult('0');
+    } else if (value === "C") {
+      setResult("0");
     } else {
-      setResult(prev => prev === '0' ? String(value) : prev + value);
+      setResult((prev) => (prev === "0" ? String(value) : prev + value));
     }
   };
-
+  const array = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "="];
+  const operator = ["-", "/", "*", "+", ".",'C'];
   return (
     <div className="calculator">
       <Display value={result} />
+      <div className="buttons top">
+        {operator.map((e) => (
+          <Buttonop key={e} value={e} onClick={handleClick} />
+        ))}
+      </div>
       <div className="buttons">
-        {[7, 8, 9, '/', 4, 5, 6, '*', 1, 2, 3, '-', 0, '.', '=', '+', 'C'].map(item => (
+        {array.map((item) => (
           <Button key={item} value={item} onClick={handleClick} />
         ))}
       </div>
